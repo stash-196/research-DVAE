a798e2e06e3d69b9887d20659e327348727dac9f
Stash Tomonaga <ratatouille.apple@gmail.com>
2023-08-05 13:10:55 +0900

    add loss function

--- dvae/utils/__init__.py
+++ dvae/utils/__init__.py
@@ -5,4 +5,4 @@
 from .logger import get_logger
 from .read_config import myconf
 from .eval_metric import EvalMetrics
-from .loss import loss_ISD, loss_KLD, loss_JointNorm, loss_MPJPE
+from .loss import loss_ISD, loss_KLD, loss_JointNorm, loss_MPJPE, loss_MSE
