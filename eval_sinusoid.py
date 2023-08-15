fe1c6ea03bef5d2f9860ade36c950cd08447bc97
Stash Tomonaga <ratatouille.apple@gmail.com>
2023-08-15 13:34:39 +0900

    add sinusoid .ini and eval_sinusoid.py

--- eval_sinusoid.py
+++ eval_sinusoid.py
@@ -14,8 +14,9 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from dvae.learning_algo import LearningAlgorithm
 from dvae.learning_algo_ss import LearningAlgorithm_ss
-from dvae.dataset.h36m_dataset import HumanPoseXYZ
-from dvae.utils import loss_MPJPE, EvalMetrics
+from dvae.dataset import sinusoid_dataset
+from dvae.utils import EvalMetrics
+from torch.nn.functional import mse_loss
 
 torch.manual_seed(0)
 np.random.seed(0)
@@ -30,8 +31,6 @@ class Options:
         self.parser.add_argument('--ss', action='store_true', help='schedule sampling')
         self.parser.add_argument('--cfg', type=str, default=None, help='config path')
         self.parser.add_argument('--saved_dict', type=str, default=None, help='trained model dict')
-        # Dataset
-        self.parser.add_argument('--test_dir', type=str, default='./data/h3.6m/dataset', help='test dataset')
     def get_params(self):
         self._initial()
         self.opt = self.parser.parse_args()
@@ -55,25 +54,22 @@ dvae.eval()
 cfg = learning_algo.cfg
 print('Total params: %.2fM' % (sum(p.numel() for p in dvae.parameters()) / 1000000.0))
 
-
-test_dataset = HumanPoseXYZ(path_to_data=params['test_dir'], 
-                            seq_len=50, split=1, actions=None, sample_rate=2,
-                            skip_rate=2, val_indices=64, h36m13kpts=None)
-test_num = test_dataset.__len__()
+# Use the sinusoid dataset
+train_dataloader, val_dataloader, train_num, val_num = sinusoid_dataset.build_dataloader(cfg)
+test_num = len(val_dataloader.dataset)
 print('Test samples: {}'.format(test_num))
-test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=128,
-                                            shuffle=True, num_workers=8)
 
-MPJPE = 0
-for _, batch_data in tqdm(enumerate(test_dataloader)):
+RMSE = 0
+for _, batch_data in tqdm(enumerate(val_dataloader)):
 
     batch_data = batch_data.to('cuda')
-    batch_data = batch_data.permute(1, 0, 2) / 1000 # normalize to meters
+    # (batch_size, seq_len, x_dim) -> (seq_len, batch_size, x_dim)
+    batch_data = batch_data.permute(1, 0, 2)
     recon_batch_data = dvae(batch_data)
-    loss_recon = loss_MPJPE(batch_data*1000, recon_batch_data*1000) # sum over seq_Len and batch size
-
+    
+    loss_recon = torch.sqrt(mse_loss(batch_data, recon_batch_data))  # Compute RMSE
     seq_len = batch_data.shape[0]
-    MPJPE += loss_recon.item() / seq_len
+    RMSE += loss_recon.item() / seq_len
 
-MPJPE = MPJPE / test_num
-print('MPJPE: {:.2f}'.format(MPJPE))
+RMSE = RMSE / test_num
+print('RMSE: {:.2f}'.format(RMSE))
