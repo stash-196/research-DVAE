ea6f4ec2243843c632871d32385b0fb4085da848
Stash Tomonaga <ratatouille.apple@gmail.com>
2023-08-16 00:43:11 +0900

    update eval_sin visualize multi timescale h

--- eval_sinusoid.py
+++ eval_sinusoid.py
@@ -79,27 +79,35 @@ def spectral_analysis(true_data, recon_data, save_dir):
     plt.savefig(fig_file)
     plt.close()
 
-def visualize_hidden_states(h_states, save_dir, name=''):
+def visualize_hidden_states(h_states, save_dir, name='', alphas=None):
     plt.figure(figsize=(12, 8))
     
     num_dims = h_states.shape[2]
-    colors = plt.cm.viridis(np.linspace(0, 1, num_dims))
     
+    # If alphas are provided, determine unique colors based on unique alphas.
+    if alphas is not None:
+        colors = [plt.cm.viridis(alpha.item()) for alpha in alphas]
+    else:
+        colors = plt.cm.viridis(np.linspace(0, 1, num_dims))
+  
     # Given h_states is of shape (seq_len, batch_size, hidden_size)
     # For this example, we are plotting for batch 0 and all hidden dimensions
     for dim in range(num_dims):
         plt.plot(h_states[:, 0, dim].cpu().numpy(), label=f'Dim {dim}', color=colors[dim])
+    
+    str_alphas = ' α:' + str(set(alphas.numpy())) if alphas is not None else ''
 
-    plt.title('Evolution of Hidden States over Time')
+    plt.title('Evolution of Hidden States over Time' + str_alphas)
     plt.xlabel('Time steps')
     plt.ylabel('Hidden state value')
     if num_dims <= 10:
-        plt.legend(loc='upper right', bbox_to_anchor=(1.15, 1))
+        plt.legend(loc='upper right', bbox_to_anchor=(1.25, 1))
     plt.grid(True)
 
     fig_file = os.path.join(save_dir, f'vis_hidden_state_evolution{name}.png')
     plt.savefig(fig_file, bbox_inches='tight')
     plt.close()
+
 
 
 
@@ -130,6 +138,11 @@ if __name__ == '__main__':
     train_dataloader, val_dataloader, train_num, val_num = sinusoid_dataset.build_dataloader(cfg, device)
     test_num = len(val_dataloader.dataset)
     print('Test samples: {}'.format(test_num))
+
+    # Check if "alpha" exists in the config.ini under the [Network] section
+    alphas_per_unit = None
+    if 'Network' in cfg and 'alphas' in cfg['Network']:
+        alphas_per_unit = dvae.alphas_per_unit
 
     RMSE = 0
     MSE = 0
@@ -164,7 +177,7 @@ if __name__ == '__main__':
                 visualize_sequences(true_series_avg, recon_series_avg, os.path.dirname(params['saved_dict']), '_avg')
 
                 # visualize the hidden states
-                visualize_hidden_states(dvae.h, os.path.dirname(params['saved_dict']))
+                visualize_hidden_states(dvae.h, os.path.dirname(params['saved_dict']), alphas=alphas_per_unit)
 
 
 
