f9bfb53b29484d79547caf6cdc13ad8882f1101a
Stash Tomonaga <ratatouille.apple@gmail.com>
2023-08-15 23:07:39 +0900

    update eval sin to auto find .ini; add eval lorenz63

--- eval_lorenz63.py
+++ eval_lorenz63.py
@@ -24,10 +24,9 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from dvae.learning_algo import LearningAlgorithm
 from dvae.learning_algo_ss import LearningAlgorithm_ss
-from dvae.dataset import sinusoid_dataset
-from dvae.utils import EvalMetrics, loss_MSE
+from dvae.dataset import lorenz63_dataset
+from dvae.utils import EvalMetrics
 from torch.nn.functional import mse_loss
-
 
 class Options:
     def __init__(self):
@@ -43,11 +42,6 @@ class Options:
         self._initial()
         self.opt = self.parser.parse_args()
         params = vars(self.opt)
-
-        # If cfg is not provided, set it based on the directory of saved_dict
-        if params['cfg'] is None:
-            params['cfg'] = os.path.join(os.path.dirname(params['saved_dict']), 'config.ini')
-        
         return params
 
 def visualize_sequences(true_data, recon_data, save_dir, name=''):
@@ -82,25 +76,20 @@ def spectral_analysis(true_data, recon_data, save_dir):
 def visualize_hidden_states(h_states, save_dir, name=''):
     plt.figure(figsize=(12, 8))
     
-    num_dims = h_states.shape[2]
-    colors = plt.cm.viridis(np.linspace(0, 1, num_dims))
-    
     # Given h_states is of shape (seq_len, batch_size, hidden_size)
     # For this example, we are plotting for batch 0 and all hidden dimensions
-    for dim in range(num_dims):
-        plt.plot(h_states[:, 0, dim].cpu().numpy(), label=f'Dim {dim}', color=colors[dim])
+    for dim in range(h_states.shape[2]):
+        plt.plot(h_states[:, 0, dim].cpu().numpy(), label=f'Dim {dim}')
 
     plt.title('Evolution of Hidden States over Time')
     plt.xlabel('Time steps')
     plt.ylabel('Hidden state value')
-    if num_dims <= 10:
-        plt.legend(loc='upper right', bbox_to_anchor=(1.15, 1))
+    plt.legend(loc='upper right', bbox_to_anchor=(1.15, 1))
     plt.grid(True)
 
     fig_file = os.path.join(save_dir, f'vis_hidden_state_evolution{name}.png')
     plt.savefig(fig_file, bbox_inches='tight')
     plt.close()
-
 
 
 if __name__ == '__main__':
@@ -126,14 +115,12 @@ if __name__ == '__main__':
     cfg = learning_algo.cfg
     print('Total params: %.2fM' % (sum(p.numel() for p in dvae.parameters()) / 1000000.0))
 
-    # Use the sinusoid dataset
-    train_dataloader, val_dataloader, train_num, val_num = sinusoid_dataset.build_dataloader(cfg, device)
+    # Use the lorenz63 dataset
+    train_dataloader, val_dataloader, train_num, val_num = lorenz63_dataset.build_dataloader(cfg, device)
     test_num = len(val_dataloader.dataset)
     print('Test samples: {}'.format(test_num))
 
     RMSE = 0
-    MSE = 0
-    MY_MSE = 0
     with torch.no_grad():
         for i, batch_data in tqdm(enumerate(val_dataloader)):
 
@@ -142,14 +129,9 @@ if __name__ == '__main__':
             batch_data = batch_data.permute(1, 0, 2)
             recon_batch_data = dvae(batch_data)
             
-            rmse_recon = torch.sqrt(mse_loss(batch_data, recon_batch_data))  # Compute RMSE
-            mse_recon = mse_loss(batch_data, recon_batch_data)  # Compute MSE
-            MY_MSE = loss_MSE(batch_data, recon_batch_data)  # Compute MSE
-
-            seq_len, batch_size, x_dim = batch_data.shape
-            RMSE += rmse_recon.item() / (seq_len * batch_size)
-            MSE += mse_recon.item() / (seq_len * batch_size)
-            MY_MSE += MY_MSE.item() / (seq_len * batch_size)
+            loss_recon = torch.sqrt(mse_loss(batch_data, recon_batch_data))  # Compute RMSE
+            seq_len = batch_data.shape[0]
+            RMSE += loss_recon.item() / seq_len
 
             if i == 0:
                 true_series = batch_data[0:10, 0, :].reshape(-1).cpu().numpy()
@@ -172,13 +154,9 @@ if __name__ == '__main__':
 
     RMSE = RMSE / test_num
     print('RMSE: {:.2f}'.format(RMSE))
-    print('MSE: {:.2f}'.format(MSE))
-    print('MY_MSE: {:.2f}'.format(MY_MSE))
 
     results_path = os.path.join(os.path.dirname(params['saved_dict']), 'results.txt')
     with open(results_path, 'w') as f:
         f.write('RMSE: {:.2f}\n'.format(RMSE))
-        f.write('MSE: {:.2f}\n'.format(MSE))
-        f.write('MY_MSE: {:.2f}\n'.format(MY_MSE))
 
     # Plot the reconstruction
