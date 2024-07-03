c02ac9ecec79d44e7c9981375de25301fc8a3c5a
stash-196 <ratatouille.apple@gmamil.com>
2024-07-03 14:43:31 +0900

    update zamami bash script

--- run_scripts/bash/run_aggregate_and_visualize.sh
+++ run_scripts/bash/run_aggregate_and_visualize.sh
@@ -10,7 +10,8 @@ echo "Running on host: `hostname`"
 cd ~/workspace/research-DVAE
 
 # Directory to start searching from
-saved_model_dir="/Users/stashtomonaga/workspace/research-DVAE/saved_model/"
+# saved_model_dir="/Users/stashtomonaga/workspace/research-DVAE/saved_model/"
+saved_model_dir="/home/stash/storage/research-DVAE/saved_model"
 
 for date_dir in "$saved_model_dir"/*; do
     echo "[bash] Checking date directory: $date_dir"
@@ -18,7 +19,16 @@ for date_dir in "$saved_model_dir"/*; do
         # Iterate over subdirectories in each date directory
         for exp_dir in "$date_dir"/*; do
             echo "[bash] Checking experiment directory: $exp_dir"
-            /Users/stashtomonaga/workspace/research-DVAE/research-DVAE/bin/python aggregate_and_visualize_metrics2.py --exp_dir "$exp_dir"        
+            # /Users/stashtomonaga/workspace/research-DVAE/research-DVAE/bin/python aggregate_and_visualize_metrics2.py --exp_dir "$exp_dir"
+
+            SIF_FILE=~/containers/dvae_cuda11.8_py3.10.sif
+            # run on HPC
+            # apptainer exec --nv $SIF_FILE python3 aggregate_and_visualize_metrics2.py --exp_dir "$exp_dir"
+
+            exp_dirname=$(basename "$exp_dir")
+            log_file=run_scripts/temp/eval_$exp_dirname.log
+            # run on HPC with nohup
+            nohup apptainer exec --nv $SIF_FILE python3 aggregate_and_visualize_metrics2.py --exp_dir "$exp_dir" > $log_file &
         done
     fi
 done
--- run_scripts/bash/run_eval_multiple.sh
+++ run_scripts/bash/run_eval_multiple.sh
@@ -5,27 +5,37 @@ echo "Time BEGIN: `date`"
 echo "Running on host: `hostname`"
 
 # source activate environment_name
-# SIF_FILE = ~/workspace/research-DVAE/my_container.sif
+SIF_FILE=~/containers/dvae_cuda11.8_py3.10.sif
+echo "[bash] Using Singularity Image: $SIF_FILE"
 
 cd ~/workspace/research-DVAE
 
 # Directory to start searching from
-search_dir="/Users/stashtomonaga/workspace/research-DVAE/saved_model/2024-06-27"
+# search_dir="/Users/stashtomonaga/workspace/research-DVAE/saved_model/2024-06-27"
+search_dir="/home/stash/storage/research-DVAE/saved_model"
+echo "[bash] Searching for models in directory: $search_dir"
 
 # Find all files matching *final*.pt recursively starting from search_dir
 find "$search_dir" -type f -name "*final*.pt" | while read final_model; do
     # Check if the file exists (redundant check because find only returns existing files)
     if [[ -f "$final_model" ]]; then
         echo "[bash] Running evaluation for model: $final_model"
+        # Name of directory containing the $final_model, without the whole path
+        model_dirname=$(basename $(dirname "$final_model"))
+        log_file=run_scripts/temp/eval_$model_dirname.log
+        echo "[bash] Log file: $log_file"
         # Run the command
         # Running Locally                
         # ~/workspace/research-DVAE/bin/python eval_sinusoid.py --saved_dict "$final_model"
         # Running Locally with nohup
-        nohup /Users/stashtomonaga/workspace/research-DVAE/research-DVAE/bin/python eval_sinusoid.py --saved_dict "$final_model" > run_scripts/temp/eval_$
+        # nohup /Users/stashtomonaga/workspace/research-DVAE/research-DVAE/bin/python eval_sinusoid.py --saved_dict "$final_model" > run_scripts/temp/eval_$
         
         # Running on HPC
         # apptainer exec --nv $SIF_FILE python3 eval_sinusoid.py --saved_dict "$final_model"
-        # nohup apptainer exec --nv $SIF_FILE python3 eval_sinusoid.py --saved_dict "$final_model" > run_scripts/temp/eval_$ 
+        # Running on HPC with nohup
+        nohup apptainer exec --nv $SIF_FILE python3 eval_sinusoid.py --saved_dict "$final_model" > $log_file &
+        
+
     fi
 done
 
