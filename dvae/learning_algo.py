fix nan handling and masking in learning_algo and mt-rnn

dvae/learning_algo.py
@@ -90,6 +90,9 @@ class LearningAlgorithm:
             "Training", "auto_warm_start", fallback=0.0
         )
         self.auto_warm_limit = self.sampling_ratio
+        self.mask_autonomous_filled = self.cfg.getboolean(
+            "Training", "mask_autonomous_filled", fallback=False
+        )
 
         # Get host name and date
         self.hostname = socket.gethostname()
@@ -431,13 +434,14 @@ class LearningAlgorithm:
 
             sampling_config = sampling_configs[self.sampling_method]
 
+            torch.autograd.set_detect_anomaly(True)
             # Batch training
             for _, batch_data in enumerate(train_dataloader):
                 batch_data = batch_data.to(self.device)
                 batch_size = batch_data.size(0)
                 if batch_size != self.batch_size:
                     logger.warning(
-                        f"[Learning Algo][epoch{epoch}] batch_size: {batch_size}, expected: {self.batch_size}"
+                        f"[Learning Algo][epoch{epoch}][train] batch_size: {batch_size}, expected: {self.batch_size}"
                     )
 
                 model_mode_selector = create_autonomous_mode_selector(
@@ -446,11 +450,27 @@ class LearningAlgorithm:
                     autonomous_ratio=sampling_config["autonomous_ratio"](),
                     batch_size=batch_size,
                 )
+
+                # if nan is present in the batch data, overlay the mask to fill with autonomous mode
+                if batch_data.isnan().any():
+                    # ratio of nan
+                    nan_ratio = batch_data.isnan().sum() / batch_data.numel()
+                    logger.warning(
+                        f"[Learning Algo][epoch{epoch}][train] Nan detected in validation data: {nan_ratio}. Will overlay on mask"
+                    )
+                    # replace where nan is present with 1 to fill with autonomous mode
+                    model_mode_selector[
+                        batch_data.squeeze().T.isnan().detach().cpu()
+                    ] = 1.0
+
+                # creating tensor mask for loss function
                 teacher_forcing_timepoints_mask = 1.0 - torch.tensor(
                     model_mode_selector, device=self.device
                 ).unsqueeze(-1)
 
-                print(f"[Learning Algo][epoch{epoch}] sent to device: {self.device}")
+                print(
+                    f"[Learning Algo][epoch{epoch}][train] sent to device: {self.device}"
+                )
 
                 if self.dataset_name == "Lorenz63" or self.dataset_name == "Sinusoid":
                     # (batch_size, seq_len, x_dim) -> (seq_len, batch_size, x_dim)
@@ -462,12 +482,18 @@ class LearningAlgorithm:
                         from_instance=f"[Learning Algo][epoch{epoch}][train]",
                     )
 
-                    mask_autonomous = True
-                    if mask_autonomous:
+                    if self.mask_autonomous_filled:
                         batch_data_masked = teacher_forcing_timepoints_mask * batch_data
+                        batch_data_masked[batch_data_masked.isnan()] = 0.0
                         recon_batch_data_masked = (
                             teacher_forcing_timepoints_mask * recon_batch_data
                         )
+                        assert ~(
+                            (
+                                (batch_data_masked == 0)
+                                != (recon_batch_data_masked == 0)
+                            ).any()
+                        )  # Check that mask (==0.0 values) is identical
                         # Log percentage of masked data in loss
                         logger.info(
                             f"[Learning Algo][epoch{epoch}] Percentage of un-masked data in loss: {teacher_forcing_timepoints_mask.mean()}"
@@ -479,10 +505,9 @@ class LearningAlgorithm:
                         loss_recon = loss_MSE(batch_data, recon_batch_data)
 
                     # Raise warning if nan is detected in recon loss
-                    if torch.isnan(loss_recon).any():
-                        logger.warning(
-                            f"[Learning Algo][epoch{epoch}] Nan detected in recon loss"
-                        )
+                    assert not torch.isnan(
+                        loss_recon
+                    ).any(), f"[Learning Algo][epoch{epoch}][train] Nan detected in recon loss"
                 else:
                     logger.error("Unknown datset")
                 seq_len, bs, _ = self.model.y.shape  # Sequence Length and Batch Size
@@ -605,6 +630,20 @@ class LearningAlgorithm:
                     autonomous_ratio=sampling_config["autonomous_ratio"](),
                     batch_size=batch_size,
                 )
+
+                # if nan is present in the batch data, overlay the mask to fill with autonomous mode
+                if batch_data.isnan().any():
+                    # ratio of nan
+                    nan_ratio = batch_data.isnan().sum() / batch_data.numel()
+                    logger.warning(
+                        f"[Learning Algo][epoch{epoch}][train] Nan detected in validation data: {nan_ratio}. Will overlay on mask"
+                    )
+                    # replace where nan is present with 1 to fill with autonomous mode
+                    model_mode_selector[
+                        batch_data.squeeze().T.isnan().detach().cpu()
+                    ] = 1.0
+
+                # creating tensor mask for loss function
                 teacher_forcing_timepoints_mask = 1.0 - torch.tensor(
                     model_mode_selector, device=self.device
                 ).unsqueeze(-1)
@@ -619,8 +658,9 @@ class LearningAlgorithm:
                         logger=logger,
                         from_instance=f"[Learning Algo][epoch{epoch}][val]",
                     )
-                    if mask_autonomous:
+                    if self.mask_autonomous_filled:
                         batch_data_masked = teacher_forcing_timepoints_mask * batch_data
+                        batch_data_masked[batch_data_masked.isnan()] = 0.0
                         recon_batch_data_masked = (
                             teacher_forcing_timepoints_mask * recon_batch_data
                         )

dvae/model/mt_rnn.py
@@ -262,49 +262,56 @@ class MT_RNN(nn.Module):
                 c_t = torch.zeros(self.num_rnn, batch_size, self.dim_rnn).to(
                     self.device
                 )
+            feature_x = torch.zeros((seq_len, batch_size, self.dense_x[-1])).to(
+                self.device
+            )
         else:
             y = self.y
             h = self.h
             h_t = self.h_t
+            feature_x = self.feature_x
             if self.type_rnn == "LSTM":
                 c_t = self.c_t
 
         # main part
-        feature_x = self.feature_extractor_x(x)
+        # feature_x = self.feature_extractor_x(x)
+        mode_selector = torch.tensor(mode_selector).to(self.device)
+        # x[x.isnan()] = 0.0
 
         for t in range(seq_len):
             if mode_selector is not None:
                 # Calculate the mix of autonomous and teacher-forced inputs
-                if len(mode_selector.shape) == 1:
-                    mix_ratio = mode_selector[t] * torch.ones(batch_size).to(
-                        self.device
-                    )
-                elif len(mode_selector.shape) == 2:
-                    mix_ratio = torch.tensor(mode_selector[t, :]).to(self.device)
+                mode_selector_t = mode_selector[t].unsqueeze(-1)
             else:
-                mix_ratio = torch.ones(batch_size).to(self.device)
+                mode_selector_t = 0.0  # Default to full teacher forcing if mode_selector is not provided
 
-            # Generate features for both teacher-forced and autonomous mode
-            feature_tf = feature_x[t, :, :].unsqueeze(0)  # Teacher-forced feature
-            feature_auto = (
-                self.feature_extractor_x(y_t)
-                if t > 0
-                else feature_x[0, :, :].unsqueeze(0)
-            )  # Autonomous feature
+            input_t = x[t, :, :].unsqueeze(0).clone()
+            # create nan mask for the input
+            if input_t.isnan().any():
+                if t == 0:  # Replace NaNs with 0.0 at time step 0
+                    input_t = torch.where(
+                        input_t.isnan(),
+                        torch.tensor(0.0, device=input_t.device, dtype=input_t.dtype),
+                        input_t,
+                    )
+                else:  # Replace NaNs with y_t at time steps t > 0
+                    input_t = torch.where(input_t.isnan(), y_t, input_t)
 
-            # Mix the features based on the ratio
-            feature_xt = (
-                mix_ratio.unsqueeze(1) * feature_auto
-                + (1 - mix_ratio.unsqueeze(1)) * feature_tf
-            )
+            if t == 0:
+                feature_xt = self.feature_extractor_x(input_t)
+            else:
+                input_t = mode_selector_t * y_t + (1 - mode_selector_t) * input_t
+                # Mix the features based on the ratio
+                feature_xt = self.feature_extractor_x(input_t)
 
             # check if shape of feature_xt is correct
-            assert feature_xt.shape == (self.x_dim, batch_size, self.dense_x[-1])
+            assert feature_xt.shape == (1.0, batch_size, self.dense_x[-1])
 
             h_t_last = h_t.view(self.num_rnn, 1, batch_size, self.dim_rnn)[-1, :, :, :]
             y_t = self.generation_x(h_t_last)
             y[t, :, :] = torch.squeeze(y_t, 0)
             h[t, :, :] = torch.squeeze(h_t_last, 0)
+            feature_x[t, :, :] = torch.squeeze(feature_xt, 0)
 
             if self.type_rnn == "LSTM":
                 h_t, c_t = self.recurrence(feature_xt, h_t, c_t)  # recurrence for t+1