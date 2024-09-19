fix data masking in learning_algo; fix inplace operation breaking autograd in mt_rnn

dvae/learning_algo.py
@@ -496,7 +496,7 @@ class LearningAlgorithm:
                         )  # Check that mask (==0.0 values) is identical
                         # Log percentage of masked data in loss
                         logger.info(
-                            f"[Learning Algo][epoch{epoch}] Percentage of un-masked data in loss: {teacher_forcing_timepoints_mask.mean()}"
+                            f"[Learning Algo][epoch{epoch}][train] Percentage of un-masked data in loss: {teacher_forcing_timepoints_mask.mean()}"
                         )
                         loss_recon = loss_MSE(
                             batch_data_masked, recon_batch_data_masked
@@ -664,11 +664,28 @@ class LearningAlgorithm:
                         recon_batch_data_masked = (
                             teacher_forcing_timepoints_mask * recon_batch_data
                         )
+                        assert ~(
+                            (
+                                (batch_data_masked == 0)
+                                != (recon_batch_data_masked == 0)
+                            ).any()
+                        )  # Check that mask (==0.0 values) is identical
+                        # Log percentage of masked data in loss
+                        logger.info(
+                            f"[Learning Algo][epoch{epoch}][val] Percentage of un-masked data in loss: {teacher_forcing_timepoints_mask.mean()}"
+                        )
                         loss_recon = loss_MSE(
                             batch_data_masked, recon_batch_data_masked
                         )
                     else:
                         loss_recon = loss_MSE(batch_data, recon_batch_data)
+
+                    # Raise warning if nan is detected in recon loss
+                    assert not torch.isnan(
+                        loss_recon
+                    ).any(), (
+                        f"[Learning Algo][epoch{epoch}][val] Nan detected in recon loss"
+                    )
 
                 seq_len, bs, _ = self.model.y.shape
                 loss_recon_avg = loss_recon / (seq_len * bs)

dvae/model/mt_rnn.py
@@ -280,15 +280,31 @@ class MT_RNN(nn.Module):
 
         for t in range(seq_len):
             if mode_selector is not None:
-                # Calculate the mix of autonomous and teacher-forced inputs
-                mode_selector_t = mode_selector[t].unsqueeze(-1)
-            else:
-                mode_selector_t = 0.0  # Default to full teacher forcing if mode_selector is not provided
+                if len(mode_selector.shape) == 2:
+                    # Calculate the mix of autonomous and teacher-forced inputs
+                    mode_selector_t = (
+                        mode_selector[t].unsqueeze(-1).unsqueeze(0).float()
+                    )
+                elif len(mode_selector.shape) == 1:
+                    # fill tensor of shape (1, batch_size, x_dim) with value of mode_selector[t]
+                    mode_selector_t = (
+                        mode_selector[t].item()
+                        * torch.ones(1, batch_size, self.x_dim).to(self.device).float()
+                    )
 
-            input_t = x[t, :, :].unsqueeze(0).clone()
+            else:
+                # Default to full teacher forcing if mode_selector is not provided, shape (1, batch_size, x_dim)
+                mode_selector_t = (
+                    torch.zeros(1, batch_size, self.x_dim).to(self.device).float()
+                )
+            # print t
+            print("t: ", t)
+            input_t = x[t, :, :].unsqueeze(0)
+            assert input_t.shape == mode_selector_t.shape
             # create nan mask for the input
             if input_t.isnan().any():
                 if t == 0:  # Replace NaNs with 0.0 at time step 0
+                    # if True:
                     input_t = torch.where(
                         input_t.isnan(),
                         torch.tensor(0.0, device=input_t.device, dtype=input_t.dtype),
@@ -300,6 +316,7 @@ class MT_RNN(nn.Module):
             if t == 0:
                 feature_xt = self.feature_extractor_x(input_t)
             else:
+                assert mode_selector_t.shape == y_t.shape == input_t.shape
                 input_t = mode_selector_t * y_t + (1 - mode_selector_t) * input_t
                 # Mix the features based on the ratio
                 feature_xt = self.feature_extractor_x(input_t)